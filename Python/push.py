4f30a6816a8aab076c16dff88680371430ffe2a4
Donghyeon <ideakhan12@naver.com>
2018-05-18 01:46:50 +0900

    push.py 주석 삭제

--- Python/push.py
+++ Python/push.py
@@ -1,19 +1,7 @@
-"""
-from pyfcm import FCMNotification
-
-proxy_dict = {
-    "http" : "http://fcm.googleapis.com/fcm/send"
-    #"https" : "https://fcm.googleapis.com/fcm/send"
-}
-push_service = FCMNotification(api_key="AAAAVjUAzTk:APA91bFpZLbUpXYTCV-wOXYZyG9RIKnhkZlVu1zOHnsFglpDSyQm-tEEZSAylVDcPzin4_nz3LXFUbJlLDH9Fhkk2ZPPdvqvgiJfyxDW_J2cY8uYQlsXfDZZjuJpUEEfNR1Yr9bPCkEI", proxy_dict=proxy_dict)
-
-registration_id = "/topics/news"
-title = "Go"
-contents = "Home"
-result = push_service.notify_single_device(registration_id=registration_id, message_title=title, message_body=contents)
-
-print(result)
-"""
+'''
+제목 : push.py
+주제 : 앱에 푸시알림 전송
+'''
 
 import json
 import requests
