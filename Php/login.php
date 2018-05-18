a9c67156e1e148a370382ddf9101d3aa58324a3c
yeeun park <charm5231@naver.com>
2018-05-18 21:51:29 +0900

    로그인 관련 login.php 파일에서 에러처리하는 부분은 후에 여유가 생기면 에러 페이지를 새로 생성하여 처리하고자 함 일단 현재는 에러 메시지만 출력하도록 구현

---
+++ Php/login.php
@@ -0,0 +1,36 @@
+﻿<?php
+include('config.php');
+
+session_start();
+
+$id = $_POST['id'];
+$pwd = $_POST['pwd'];
+
+$sql = "SELECT * FROM members WHERE id='$id'";
+$result = $con->query($sql);
+
+if($result->num_rows==1)
+{
+	$row=$result->fetch_array(MYSQLI_ASSOC);
+	if($row['password'] == $pwd)
+	{
+		$_SESSION['id'] = $id;
+		if(isset($_SESSION['id']))
+		{
+			header('Location: ./index.php');
+		}
+		else
+		{
+			echo "세션 저장 실패";
+		}
+	}
+	else
+	{
+		echo "wrong id or pw";
+	}
+}
+else
+{
+	echo "wrong id or pwd";
+}
+?>
